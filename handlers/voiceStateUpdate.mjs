959434e587ca9c9ea7665855d3a529b9da57065a
NepiaaaaN <surface095@gmail.com>
2024-10-21 22:32:26 +0900

    Add leave room message

--- handlers/voiceStateUpdate.mjs
+++ handlers/voiceStateUpdate.mjs
@@ -1,27 +1,62 @@
 import { EmbedBuilder } from "discord.js";
 
-import Notification from "../models/notification.mjs"
+import Notification from "../models/notification.mjs";
 
 export default async (oldState, newState) => {
-  if (oldState.channelId === null && newState.channel?.members.size == 1){
-    const notifications = await Notification.findAll({
-      where: {
-        guildId: newState.guild.id,
-        voiceChannelId: newState.channel.id,
-      },
-    });
-    
-    const embed = new EmbedBuilder()
-      .setColor(0x5cb85c)
-      .setAuthor({ name: newState.member.displayName, iconURL: newState.member.displayAvatarURL()})
-      .setTitle(`<#${newState.channel.id}> で通話を開始しました！`)
-      .setTimestamp();
-    
-    await Promise.all(
-      notifications.map(async n => {
-        const channel = await newState.guild.channels.fetch(n.textChannelId);
-        await channel.send({ embeds: [embed] });
-      })
+  if (newState.channelId != null && oldState.channelId == null) {
+    // 入室処理
+    const title = `<#${newState.channel.id}> で通話を開始しました！`;
+    await sendNotification(
+      newState.member,
+      newState.channel.id,
+      newState.channel.id,
+      newState.guild.id,
+      0x5cb85c,
+      title
+    );
+  } else if (oldState.channelId != null && newState.channelId == null) {
+    // 退出処理
+    const title = `<#${oldState.channel.id}> から退出しました`;
+    await sendNotification(
+      oldState.member,
+      oldState.channel.id,
+      oldState.channel.id,
+      oldState.guild.id,
+      0xff6347,
+      title
     );
   }
 };
+
+// 通知を送信する関数
+async function sendNotification(
+  member,
+  channelId,
+  voiceChannelId,
+  guildId,
+  color,
+  title
+) {
+  const notifications = await Notification.findAll({
+    where: {
+      guildId: guildId,
+      voiceChannelId: voiceChannelId,
+    },
+  });
+
+  const embed = new EmbedBuilder()
+    .setColor(color)
+    .setAuthor({
+      name: member.displayName,
+      iconURL: member.displayAvatarURL(),
+    })
+    .setTitle(title)
+    .setTimestamp();
+
+  await Promise.all(
+    notifications.map(async (n) => {
+      const channel = await member.guild.channels.fetch(n.textChannelId);
+      await channel.send({ embeds: [embed] });
+    })
+  );
+}
