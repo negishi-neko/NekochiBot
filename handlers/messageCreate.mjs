49aa8a035f0b500c9d5ecfdc8630193618049722
NepiaaaaN <surface095@gmail.com>
2025-04-05 21:25:04 +0900

    Add yasuba bot

--- handlers/messageCreate.mjs
+++ handlers/messageCreate.mjs
@@ -10,4 +10,23 @@ export default async (message) => {
   if (message.content.match(/褒めて/)) {
     await message.reply("すごい！");
   }
+
+  // 安場 bot
+  if (message.content.match(/やすば|やすちん|安場悠斗|やっすん|安場/)) {
+    const res = [
+      "なにぃ？",
+      "今集中しててんけど。",
+      "言ってくれれば工具貸したのに。",
+      "夜勤やから無理。",
+      "聞こえてるから、そんなでかい声出すなよ。",
+      "はぁ？ それくらい自分でなんとかしろよ。",
+      "興味ねぇし。",
+      "お前のせいで、集中できんやろ。",
+      "前川さぁ.....",
+      "マジでどうてもいい",
+      "(無視)",
+    ];
+    const choice = res[Math.floor(Math.random() * res.length)];
+    await message.reply(choice);
+  }
 };
